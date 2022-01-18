4ca8be92b6b20e9c236897fd3fc6fcdc4242975a
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-01-17 20:48:47 -0500

    commit tree f604ea3a575fb0e17b9a5593357afef7da329d79

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    const text=document.querySelector("#text")
+    return text.textContent="This is really cool!"
+})
+
+console.log(
+    "This console.log() fired when inde.js loads - before DOMContentLoaded is loaded"
+)
